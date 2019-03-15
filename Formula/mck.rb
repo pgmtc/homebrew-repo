733e034c22a5e6a10bc6c5cd820b71080bc2a3d9
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2019-03-15 22:09:52 +0000

    fixing mck formula

--- Formula/mck.rb
+++ Formula/mck.rb
@@ -1,9 +1,9 @@
 class Mck < Formula
   desc "mck - cli tool for mocking external dependencies and microservices"
   homepage "https://gitlab.com/pgmtc/mck"
-  url "https://gitlab.com/pgmtc/mck/-/jobs/178670747/artifacts/raw/dist/0.0.1/mck-0.0.1_darwin_amd64.tar.gz"
+  url "https://gitlab.com/pgmtc/mck/-/jobs/178674043/artifacts/raw/dist/0.0.1/mck-0.0.1_darwin_amd64.tar.gz"
   version "0.0.1"
-  sha256 "e0e6276246444fb7b8005cf0096c6546c58474a5a081f71eb6c5b97e6ef67774"
+  sha256 "906fed00420b8c14b62d73e2b03060e6dacbe09bea3a6caeb80e4464cc9afb67"
 
   def install
     bin.install "mck"
