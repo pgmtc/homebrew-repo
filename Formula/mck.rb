5465d03a47a76152c1053a1af5b99cdeefa7b0a6
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2019-03-17 22:10:30 +0000

    release of nocicd

--- Formula/mck.rb
+++ Formula/mck.rb
@@ -1,9 +1,9 @@
 class Mck < Formula
   desc "mck - cli tool for mocking external dependencies and microservices"
   homepage "https://gitlab.com/pgmtc/mck"
-  url "https://gitlab.com/pgmtc/mck/-/jobs/178682565/artifacts/raw/dist/mck-0.0.1-darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "2c298745dab5cbb676b97f8933cbd92e182db99e8ee9e06f9d08106c6a1bef5b"
+  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/mck/mck-nocicd-darwin_amd64.tar.gz"
+  version "nocicd"
+  sha256 "ff8e412e0ba29d524da7c005ea8ca2838649ac02ef36b282cc5fcd7832d38375"
 
   def install
     bin.install "mck"
