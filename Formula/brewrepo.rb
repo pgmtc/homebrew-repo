cfcdedf378b4b8c1333a9f49b5766a3d155a07fe
gitlab ci/cd <cicd@gitlab>
2019-03-20 20:55:31 +0000

    release of s3copy 0.0.1h

--- Formula/brewrepo.rb
+++ Formula/brewrepo.rb
@@ -1,1 +1,11 @@
-initial commit
+class Brewrepo < Formula
+  desc "brewrepo - tool for publishing binary to brew repository"
+  homepage "https://gitlab.com/pgmtc/citools"
+  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/brewrepo/brewrepo-0.0.1h-darwin_amd64.tar.gz"
+  version "0.0.1h"
+  sha256 "edb71d7ccbfe646bf1b6f85aee23bc4eb959113a94295f3ad38daa227a956a56"
+
+  def install
+    bin.install "brewrepo"
+  end
+end
