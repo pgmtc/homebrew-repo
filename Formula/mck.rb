11203228a5743f802f385c087e1f911a7ab46a60
gitlab ci/cd <cicd@gitlab>
2019-03-20 10:31:54 +0000

    release of 0.0.1l

--- Formula/mck.rb
+++ Formula/mck.rb
@@ -1,9 +1,9 @@
 class Mck < Formula
   desc "mck - cli tool for mocking external dependencies and microservices"
   homepage "https://gitlab.com/pgmtc/mck"
-  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/mck/mck-0.0.1h-darwin_amd64.tar.gz"
-  version "0.0.1h"
-  sha256 "54730dc63008186ecf71489ba63785487e8a9e3675d2fa1f9f620f3693560745"
+  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/mck/mck-0.0.1l-darwin_amd64.tar.gz"
+  version "0.0.1l"
+  sha256 "2750b996faf4895128a795031deb931d20e864ad27d319354da6c268417eef4f"
 
   def install
     bin.install "mck"
