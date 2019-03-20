e2fdb850bc33f4b7e39c2c89839f20dd3232b150
gitlab ci/cd <cicd@gitlab>
2019-03-20 20:55:11 +0000

    release of s3copy 0.0.1h

--- Formula/s3copy.rb
+++ Formula/s3copy.rb
@@ -1,9 +1,9 @@
 class S3copy < Formula
-  desc "s3copy - "
+  desc "s3copy - tool for pushing files to s3"
   homepage "https://gitlab.com/pgmtc/citools"
-  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/s3copy/s3copy-0.0.1g-darwin_amd64.tar.gz"
-  version "0.0.1g"
-  sha256 "fb4003f6b1329c8b703016c0482842c91448dc5e73ddbba09377b8cc05bcde1e"
+  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/s3copy/s3copy-0.0.1h-darwin_amd64.tar.gz"
+  version "0.0.1h"
+  sha256 "934014850686e4525c0146a3f770965319ffcf747583f929c8fb5311a6dc23ce"
 
   def install
     bin.install "s3copy"
