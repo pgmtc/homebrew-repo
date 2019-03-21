b949cfc271fa8cfc9f3d7eeaf3f95a17b6b6933c
gitlab ci/cd <cicd@pgmtc>
2019-03-21 21:25:55 +0000

    mck release 0.0.1p

--- Formula/mck.rb
+++ Formula/mck.rb
@@ -4,7 +4,7 @@ class Mck < Formula
   homepage "https://gitlab.com/pgmtc/mck"
   url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/mck/mck-0.0.1p-darwin_amd64.tar.gz"
   version "0.0.1p"
-  sha256 "dist/mck-0.0.1p-darwin_amd64.tar.gz"
+  sha256 "\xe2\xd9D\xe86\x90\xe6\xcd\xe1\x15\xe9]\x1dbs\xc2K\x91\xd7\xdc\xff`\xaa\xe4j%x\x84Un_\xfc"
   def install
     bin.install "mck"
   end
