881a435a6468c9c962a968677c4b086d8111a600
gitlab ci/cd <cicd@pgmtc>
2019-03-23 23:43:19 +0000

    ci release 0.0.3

--- Formula/ci.rb
+++ Formula/ci.rb
@@ -2,9 +2,9 @@
 class Ci < Formula
   desc "set of tools for easier CI/CD"
   homepage "https://gitlab.com/pgmtc/citools"
-  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/ci/ci-0.0.2g-darwin_amd64.tar.gz"
-  version "0.0.2g"
-  sha256 "41048f72bf22d8b270de156b7ca38025e6ebef881cbf6adf2180154d7717e078"
+  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/ci/ci-0.0.3-darwin_amd64.tar.gz"
+  version "0.0.3"
+  sha256 "79768c02a9bf454602e9dcaa5f0a37c971fd55931f0bf32a43ace3127abf0c6a"
   def install
     bin.install "ci"
   end
