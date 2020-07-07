143e0768ddf799dd747a869ef6bd210c3b7d77f8
gitlab ci/cd <cicd@pgmtc>
2020-07-07 16:47:58 +0000

    le release 1.5.1

--- Formula/le.rb
+++ Formula/le.rb
@@ -2,9 +2,9 @@
 class Le < Formula
   desc "le - tool for running local environments"
   homepage "https://gitlab.com/pgmtc/le"
-  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/le/le-1.5.0-darwin_amd64.tar.gz"
-  version "1.5.0"
-  sha256 "5588102642a966bd79ac94bc9e1659ee97dd5d6d8b9d4dafc6176e782c299798"
+  url "https://s3.eu-west-2.amazonaws.com/pgmtc-releases/le/le-1.5.1-darwin_amd64.tar.gz"
+  version "1.5.1"
+  sha256 "ed2dafdf0870a83c884c4a4520a55f38d96cffbdf0a71993b58d856184a3a0d5"
   def install
     bin.install "le"
   end
